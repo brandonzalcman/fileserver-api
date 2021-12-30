35c762970c0569fa8d9b2831620ccf13e323774c
Brandon <brandonzalcman@gmail.com>
2021-12-30 19:50:46 +1100

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+)
+
+func urlHandler(w http.ResponseWriter, r *http.Request) {
+	back := r.URL.Query().Get("back")
+	song := r.URL.Query().Get("song")
+	if len(back) != 0 {
+		http.ServeFile(w, r, "assets/backgrounds/"+back)
+	} else if len(song) != 0 {
+		http.ServeFile(w, r, "assets/songs/"+song)
+	} else {
+		http.NotFound(w, r)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/assets", urlHandler)
+	http.ListenAndServe(":1000", nil)
+}
